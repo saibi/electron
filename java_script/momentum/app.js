eae9622aa74c52a02388cf87f4314b16673a3528
Youngmin Kim <kimyoungmin@gmail.com>
2023-05-23 08:58:39 +0900

    nomadcoder:js - conditional

--- java_script/momentum/app.js
+++ java_script/momentum/app.js
@@ -2,6 +2,16 @@ const age = prompt("How old are you?");
 
 console.log(typeof age);
 console.log(age);
-
 const num = parseInt(age);
-console.log(isNaN(num));
+
+if (isNaN(num)) {
+  console.log("please write a number");
+} else if (num < 18) {
+  console.log("You are too young.");
+} else if (num >= 18 && num <= 60) {
+  console.log("You can drink.");
+} else if (num > 60) {
+  console.log("You should exercise.");
+} else {
+  console.log("You can do whatever you want to.");
+}
