92a3d61320ccbc1cc8857df67537cc773ac5720d
Youngmin Kim <kimyoungmin@gmail.com>
2023-05-24 19:56:30 +0900

    nomadcoder: js - openweathermap

---
+++ java_script/interval/js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "b1b87d52a9c3e19efb53742a0999ddd3";
+
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+function onGeoOK(position) {
+  let lat = position.coords.latitude;
+  let lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      city.innerHTML = data.name;
+      weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("can't find you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOK, onGeoError);
