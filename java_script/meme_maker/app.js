98f0df3dc3d3ba1344116e19e3f6b89e7fc7aacf
Youngmin Kim <kimyoungmin@gmail.com>
2023-05-26 18:37:16 +0900

    nomadcoder: js - mouse event

--- java_script/meme_maker/app.js
+++ java_script/meme_maker/app.js
@@ -4,26 +4,28 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 100, 50, 200);
-ctx.fillRect(400, 100, 50, 200);
 ctx.lineWidth = 2;
-ctx.fillRect(300, 200, 50, 100);
-ctx.fillRect(200, 100, 200, 20);
-ctx.moveTo(200, 100);
-ctx.lineTo(325, 00);
-ctx.lineTo(450, 100);
-ctx.fill();
 
-ctx.beginPath();
-ctx.fillRect(210 - 40, 400, 15, 100);
-ctx.fillRect(350 - 40, 400, 15, 100);
-ctx.fillRect(260 - 40, 400, 60, 200);
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+];
 
-ctx.arc(250, 350, 50, 0, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(event.offsetX, event.offsetY);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260, 330, 10, Math.PI, 2 * Math.PI);
-ctx.arc(220, 330, 10, Math.PI, 2 * Math.PI);
-ctx.fill();
+//canvas.addEventListener("click", onClick);
+canvas.addEventListener("mousemove", onClick);
