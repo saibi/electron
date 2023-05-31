08de0b165e1a5bb67ec27a99d80cdb0872b059a1
Youngmin Kim <kimyoungmin@gmail.com>
2023-05-31 09:02:13 +0900

    jonas course: ~12

--- java_script/jonas_course/01-Fundamentals-Part-1/script.js
+++ java_script/jonas_course/01-Fundamentals-Part-1/script.js
@@ -1,3 +1,33 @@
+/*
 let js = "amazing";
-if (js === "amazing") alert("javascript is fun");
 console.log(1 + 2 + 3 + 4 + 5);
+console.log("jonas");
+console.log(js);
+
+let PI = 3.14;
+let myName = "saibi";
+
+let myCountry = "Korea";
+let myContinent = "Asia";
+let myPopulation = 50000000;
+console.log(myCountry);
+console.log(myContinent);
+console.log(myPopulation);
+*/
+let javascriptIsFun = true;
+console.log(typeof false);
+console.log(typeof 23);
+console.log(typeof "hello");
+console.log(typeof javascriptIsFun);
+javascriptIsFun = "yes";
+console.log(typeof javascriptIsFun);
+javascriptIsFun = 1;
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+
+year = 2023;
+console.log(typeof year);
+
+console.log(typeof null);
