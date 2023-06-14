dd642d3e3c87489c6e8994a08c308cd049ec2912
Youngmin Kim <kimyoungmin@gmail.com>
2023-06-14 18:53:10 +0900

    jonas section03: ~32, javascript strict mode "use strict";

---
+++ java_script/jonas_course/02-Fundamentals-Part-2/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+const interface = 'Audio';
+const if = 23;
