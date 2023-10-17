789cdf52ceff1075c63b733bc811315c65af895d
Youngmin Kim <kimyoungmin@gmail.com>
2023-10-17 13:25:57 +0900

    apps script #4: server side js, build select options

--- apps_script/basic_webapp/Code.js
+++ apps_script/basic_webapp/Code.js
@@ -1,16 +1,25 @@
+const url =
+  "https://docs.google.com/spreadsheets/d/1kY30eXsn4Ym_FJe4Nat1g4yDVh589quFqquiwC104Xs/edit#gid=387912833";
+
 function doGet(e) {
   Logger.log(e);
 
-  // css, script 파일을 분리했기때문에 단순 page 파일 리턴 대신 전체 page 를 생성해서 리턴해야 한다.
-  //return HtmlService.createHtmlOutputFromFile("page");
-  return HtmlService.createTemplateFromFile("page").evaluate();
+  let ss = SpreadsheetApp.openByUrl(url);
+  let ws = ss.getSheetByName("basic_webapp_options");
+  let list = ws
+    .getRange(1, 1, ws.getRange("A1").getDataRegion().getLastRow(), 1)
+    .getValues();
+
+  var tmp = HtmlService.createTemplateFromFile("page");
+  tmp.list = list.map(function (r) {
+    return r[0];
+  });
+  return tmp.evaluate();
 }
 
 function userClicked(userInfo) {
   Logger.log(`${JSON.stringify(userInfo)} clicked the Button.`);
 
-  const url =
-    "https://docs.google.com/spreadsheets/d/1kY30eXsn4Ym_FJe4Nat1g4yDVh589quFqquiwC104Xs/edit#gid=387912833";
   let ss = SpreadsheetApp.openByUrl(url);
   let ws = ss.getSheetByName("basic_webapp");
 
