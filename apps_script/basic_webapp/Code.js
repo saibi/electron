056035c9ee03222ddc9b37898bb4684bfae0357b
Youngmin Kim <kimyoungmin@gmail.com>
2023-10-17 09:13:25 +0900

    apps script #2: passing object

--- apps_script/basic_webapp/Code.js
+++ apps_script/basic_webapp/Code.js
@@ -1,18 +1,21 @@
 function doGet(e) {
-  Logger.log("log test");
   Logger.log(e);
 
   return HtmlService.createHtmlOutputFromFile("page");
 }
 
-function userClicked(name) {
-  Logger.log(name + " clicked the Button.");
-  Logger.log(`${name} clicked the Button. formatting test`);
+function userClicked(userInfo) {
+  Logger.log(`${JSON.stringify(userInfo)} clicked the Button.`);
 
   const url =
     "https://docs.google.com/spreadsheets/d/1kY30eXsn4Ym_FJe4Nat1g4yDVh589quFqquiwC104Xs/edit#gid=387912833";
   let ss = SpreadsheetApp.openByUrl(url);
   let ws = ss.getSheetByName("basic_webapp");
 
-  ws.appendRow([new Date(), name]);
+  ws.appendRow([
+    userInfo.firstName,
+    userInfo.lastName,
+    userInfo.app,
+    new Date(),
+  ]);
 }
