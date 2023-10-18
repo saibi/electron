9aa913fb0150cf4bc85c4a70e445d51790fb913e
Youngmin Kim <kimyoungmin@gmail.com>
2023-10-18 10:54:23 +0900

    apps script #9: materialcss autocomplete, sheet getDataRegion()

--- apps_script/basic_webapp7/funcs.js
+++ apps_script/basic_webapp7/funcs.js
@@ -1,4 +1,3 @@
-
 function userClicked(userInfo) {
   Logger.log(`${JSON.stringify(userInfo)} clicked the Button.`);
 
@@ -52,4 +51,16 @@ function getCalendarBusyDays() {
   let uniqueDays = [...new Set(days)];
 
   return uniqueDays;
+}
+
+function getWords() {
+  let ss = SpreadsheetApp.openByUrl(url);
+  let ws = ss.getSheetByName("basic_webapp_options");
+  let data = ws.getRange(1, 3).getDataRegion().getValues();
+
+  let options = {};
+  data.forEach(function (row) {
+    options[row[0]] = null;
+  });
+  return options;
 }
