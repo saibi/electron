5eac15991bf2ea7dcb3abdd5baa7337cf243b208
Youngmin Kim <kimyoungmin@gmail.com>
2023-10-23 16:08:48 +0900

    app script crud: Search: placeholder, handle Code.gs return value, ...

--- apps_script/crud/Code.js
+++ apps_script/crud/Code.js
@@ -11,3 +11,21 @@ function loadPage(filename) {
   let html = HtmlService.createTemplateFromFile(filename);
   return html.evaluate().getContent();
 }
+
+function getDataForSearch() {
+  const ID = "1J6ziO5HBU7R9ulIvr8dENhZgKqE6p9KWRZ-PBX93lEg";
+  let ss = SpreadsheetApp.openById(ID);
+  let data = ss.getSheetByName("data").getDataRange().getValues();
+
+  let headers = data.shift();
+  let result = [];
+  data.forEach(function (row) {
+    let obj = {};
+    headers.forEach(function (header, i) {
+      obj[header] = row[i];
+    });
+    result.push(obj);
+  });
+  let ret = JSON.stringify(result);
+  return ret;
+}
